0bbd70788bef442a9778a0dba1b54c3b79c6e070
19mariusz95 <19mariusz95@gmail.com>
2016-05-04 13:21:17 +0200

    2a

---
+++ zad2/main.h
@@ -0,0 +1,13 @@
+//
+// Created by student on 03/05/16.
+//
+
+#ifndef ZAD1_MAIN_H
+#define ZAD1_MAIN_H
+
+struct record {
+    int id;
+    char text[1020];
+};
+
+#endif //ZAD1_MAIN_H
--- zad2/zad2a.c
+++ zad2/zad2a.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <zconf.h>
+#include <signal.h>
 #include "main.h"
 
 FILE *file;
@@ -11,8 +13,19 @@ int threads;
 pthread_t *thids;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int wait = 1;
+int test_number;
+int sig_number;
+
+void sighandler(int sig) {
+    printf("PID %d, TID %d, signal %d\n", getpid(), (int) pthread_self(), sig);
+}
 
 static void *thread_func(void *arg) {
+    if (test_number == 4) {
+        signal(sig_number, SIG_IGN);
+    } else if (test_number == 3 || test_number == 5) {
+        signal(sig_number, sighandler);
+    }
     while (wait);
     struct record *r = malloc(records * sizeof(struct record));
     pthread_mutex_lock(&mutex);
@@ -25,6 +38,7 @@ static void *thread_func(void *arg) {
                 fflush(stdout);
                 return (void *) 0;
             }
+            sleep(1);
         }
         pthread_mutex_lock(&mutex);
         rb = fread(r, sizeof(struct record), (size_t) records, file);
@@ -36,7 +50,7 @@ static void *thread_func(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 5) {
+    if (argc < 7) {
         printf("no enough argmuments\n");
         exit(1);
     }
@@ -46,6 +60,14 @@ int main(int argc, char *argv[]) {
     char *filename = argv[2];
     sscanf(argv[3], "%d", &records);
     word = argv[4];
+    test_number = atoi(argv[5]);
+    sig_number = atoi(argv[6]);
+
+    if (test_number == 2) {
+        signal(sig_number, SIG_IGN);
+    } else if (test_number == 3) {
+        signal(sig_number, sighandler);
+    }
 
     file = fopen(filename, "r");
     if (file == NULL) {
@@ -60,6 +82,15 @@ int main(int argc, char *argv[]) {
         }
     }
     wait = 0;
+
+    if (test_number == 1 || test_number == 2 || test_number == 3) {
+        printf("send signal %d\n", sig_number);
+        kill(getpid(), sig_number);
+    } else if (test_number == 4 || test_number == 5) {
+        printf("send signal %d to thread %d\n", sig_number, (int) thids[threads - 1]);
+        pthread_kill(thids[threads - 1], sig_number);
+    }
+
     for (int i = 0; i < threads; i++) {
         if (pthread_join(thids[i], NULL) != 0) {
             printf("error while waiting for thread\n");
